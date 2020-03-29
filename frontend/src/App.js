4a65f04aa013dbea1ce8dbd924d715a4debac27e
kaiowolf <kaioexpert@hotmail.com>
2020-03-29 04:02:42 -0300

    Frontend
    Frontend for the Application Be A Hero

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+// Quando um HTML está dentro de um arquivo JavaScript, ele é chamado de JSX (JavaScript XML)
+
+function App() {
+  
+  return (
+    <Routes />
+   );
+}
+
+export default App;
