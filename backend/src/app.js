0b27deee96580c38b950e12cab14e14975860ed3
kaiowolf <kaioexpert@hotmail.com>
2020-03-29 23:18:44 -0300

    Tests for Aplication Be A Hero
    Make some tests for Backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 /**
  * Rota / Recurso
@@ -29,4 +31,4 @@ app.use(routes);
  * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
  */
 
-app.listen(3333);
+module.exports = app;
